34cc468d097aadb5fafca7d5974e3d66c792ef06
Roeiazran <Roeiaz.per@gmail.com>
2025-11-02 19:09:30 +0200

    added remove_at

---
+++ Exercise-1/remove_at.pl
@@ -0,0 +1,7 @@
+% base case: K = 1.
+remove_at(X, [H|T], 1, R) :- X = H, R = T.
+
+% recursive step: decrement k and call remove_at on tail.
+remove_at(X, [H|T], K, [H|R]) :- 
+    K1 is K - 1,
+    remove_at(X, T, K1, R).
