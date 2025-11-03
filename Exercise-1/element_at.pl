ef63535d5307a02ff8185894bd6f51aea94636a8
Roeiazran <Roeiaz.per@gmail.com>
2025-11-03 19:37:48 +0200

    added cut

--- Exercise-1/element_at.pl
+++ Exercise-1/element_at.pl
@@ -1,11 +1,8 @@
 
 % base case: K = 1.
-element_at(X, [H|_], 1) :- X = H.
+element_at(X, [H|_], 1) :- X = H, !.
 
 % recursive step: decrement k and call element_at on tail.
 element_at(X, [_|T], K) :- 
     K1 is K - 1,
     element_at(X, T, K1).
-
-
-
