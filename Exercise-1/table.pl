2336914ce9136eb33ea42ea3bebdcc421caeb72b
Roeiazran <Roeiaz.per@gmail.com>
2025-11-03 17:32:41 +0200

    hopefully final version

--- Exercise-1/table.pl
+++ Exercise-1/table.pl
@@ -4,29 +4,22 @@ assign(false, true).
 assign(false, false).
 
 and(X,Y) :- X,Y.
+not(X) :- \+X.
+equal(X,Y) :- (X,Y); (\+X, \+Y).
+xor(X,Y) :- and(or(X,Y),not(and(X,Y))).
+nand(X,Y) :- not(and(X,Y)).
+nor(X,Y) :- not(or(X,Y)).
+or(X,Y) :- not(and(not(X), not(Y))).
 
+execute(Expr) :- call(Expr), write('true').
+execute(Expr) :- \+call(Expr), write('fail').
 write_xy(X,Y) :- write(X), write(' '), write(Y), write(' ').
 
 table(X, Y, Expr) :- 
     assign(X, Y),
     write_xy(X,Y),
-    call(Expr),
-    write('true'),
-    nl, 
-    fail.
-
-table(X, Y, Expr) :- 
-    assign(X, Y),
-    write_xy(X,Y),
-    \+ call(Expr),
-    write('false'),
+    execute(Expr),
     nl,
     fail.
 
-
-% or(X,Y) :- X;Y.
-% not(X) :- \+ X.
-% equal(X,Y) :- X =:= Y.
-% xor(X,Y) :- and(or(X,Y),not(and(X,Y))).
-% nand(X,Y) :- not(and(X,Y)).
-% nor(X,Y) :- not(or(X,Y)).
+table(_,_,_).
